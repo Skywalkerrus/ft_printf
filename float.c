ca32cdefbc3ae0d5096fcd909d9c3edc9991fc0c
mirrinat <mirzagalamov.rinat1997@mail.ru>
2019-11-25 20:57:43 +0300

    Myself,floats are working

--- float.c
+++ float.c
@@ -12,7 +12,7 @@
 
 #include "ft_printf.h"
 
-double	part_int_ten(double n, int *size)
+double	part_int_ten(double n, int size)
 {
     double mod;
 
@@ -25,106 +25,62 @@ double	part_int_ten(double n, int *size)
     return(mod);
 }
 
-void        int_part(double *n, char **str, int *i, double mod)
+int        int_part(double n, char *s, double mod)
 {
-    char    *s;
-    int     rank;
 
-    s = *str;
-    while ((int)*n != 0)
+    int     rank;
+	int i = 0;
+
+    while ((int)n != 0)
     {
-        rank = (int)(*nb / mod);
-        s[(*i)] = (char)(rank + '0');
-        *n -= rank * mod;
+        rank = (int)(n / mod);
+        s[i] = (char)(rank + '0');
+        n -= rank * mod;
         mod = mod / 10;
-        *i++;
+        i++;
     }
+	return(i);
 }
 
-void        float_part(double n, char **str, int *i, int rigor)
+int        float_part(double n, char *s, int i, int rigor)
 {
 
     int j;
     int tmp;
-    char *s;
 
-    n = n * 10;
+
+    n = (n -(int)n) * 10;
     j = 0;
-    s = *str;
-    s[(*i)++] = '.';
+    s[i++] = '.';
     while(j < rigor)
     {
-        tmp = (int)(nb);
-        s[(*i)++] = (char)(tmp + '0');
-        nb = (nb - tmp) * 10;
+		tmp = ((int)n != 9) ? (int)(n + 0.01) : (int)n;
+        s[i++] = (char)(tmp + '0');
+        n = (n - tmp) * 10;
+        j++;
     }
+	return (i);
 }
 
-int float_to_str(double n, char **res, int rigor)
-{
-    int i;
-    int size;
-    char *str;
-    double mod;
+void float_to_str(double n, char *str, int rigor) {
+	int i;
+	int size;
+	double mod;
 
-    i = 0;
-    size = 1;
-    mod = part_int_ten(b, &size);
-    str = ft_memalloc(sizeof(char) * (size + rigor));
-    int_part(&n, &str, &i, mod);
-    float_part(n, &str,&i, rigor);
-    str[--i] = '\0';
-    *res = str;
-    return (size);
+	size = 1;
+	mod = part_int_ten(n, size);
+	i = int_part(n, str, mod);
+	i = float_part(n, str, i, rigor);
+	str[i++] = '\0';
 }
 
-int write_f(void *s, int size,t_flags *flags)
-{
-    int i;
-    char *str;
+int main() {
+	char *str = (char *)malloc(sizeof(int) *1000);
 
-    i = 0;
-    str = s;
-    while (i < size)
-        flags->buffer[flags->bytes++] = str[i++];
-    flags->total_bytes += size;
-    return (size);
-}
+	float_to_str(275.1234,str,7);
+	ft_putstr(str);
 
-int type_f(va_list ap,t_flags *flags)
-{
-    char *nb;
-    int size;
-
-    size = float_to_str(va_arg(ap, double), &nb, 7);
-    write_f(nb,size,flags);
-    ft_memdel(nb);
-    return (size);
-}
-
-
-void print_float(char *hu, va_list ap)
-{
-    int size;
-
-    if(*hu == 'f')
-        size =  float_to_str(va_arg(ap,double), &nb,5);
-    write_f(nb,size,fl)
+	return(0);
 
 }
 
-//void	cast_func(char *hu, va_list ap, func *mass)
-//{
-//    m
-//}
-
-int ft_printf()
-
-int main()
-{
-
-    int size;
-
-    size = ft_put_float_to_string(va_arg(args, double), &nb, flags->precision);
-    return(0);
-}
